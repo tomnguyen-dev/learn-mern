c5a67534f0667fd9eee83e32d4738b4d7a628000
Tom <tomnguyen0817@gmail.com>
2021-04-28 20:22:53 -0500

    part1

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+const Hello = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Hello {props.fname} {props.lname}.</p>
     </div>
-  );
+  )
+}
+
+const Footer = () => {
+  return (
+    <div>
+      Greetings. App created by <a href="https://github.com/tomnguyen-dev">Tom Nguyen</a>
+    </div>
+  )
+}
+
+// const App = () => {
+//   const fname = 'Peter'
+//   const lname = 'Parker'
+//   return (
+//     <div>
+//       <h1>Greetings</h1>
+//       <Hello fname='George' lname='Johnson'/>
+//       <Hello fname='Mark' lname='Smith'/>
+//       <Hello fname={fname} lname={lname}/>
+//       <Footer/>
+//     </div>
+//   )
+// }
+
+// Using a root element is not the only working option.
+// We can also use an array of components
+// const App = () => {
+//   return [
+//     <h1>Greetings</h1>,
+//     <Hello fname='Tom' lname='Win'/>,
+//     <Footer/>
+//   ]
+// }
+
+// But when defining the root component of an application, it is
+// not a wise thing to do.
+// Because the root element is stipulated, we have extra div-elements.
+// This can be avoided by using fragments (wrapping the elements to be returned
+// by the component with an empty element)
+const App = () => {
+  const fname = 'Peter'
+  const lname = 'Parker'
+
+  return (
+    <>
+      <h1>Greetings</h1>
+      <Hello fname={fname} lname={lname}/>
+      <Footer/>
+    </>
+  )
 }
 
 export default App;
